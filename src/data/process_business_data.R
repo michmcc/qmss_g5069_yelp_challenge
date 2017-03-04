38f3acc1531ae597573a57f52f3cbd49505b8956
michmcc <michmcc@me.com>
2017-03-04 11:02:46 -0500

    Update process_business_data and subset_reviews
    Changed process_business_data.R to output a restaurant file with only
    large cities (> 1000 restaurants) and split into global and US
    
    Changed subset_reviews to take a 50% and 10% sample of the reviews from
    restaurants in the global and US restaurant files output from
    process_business_data.R

--- src/data/process_business_data.R
+++ src/data/process_business_data.R
@@ -32,9 +32,17 @@ print("creating dummy variables")
 business$restaurant <- unlist(lapply(business$categories_l, function(x) {unlist(ifelse("Restaurants" %in% x, 1, 0))}))
 
 agg <- aggregate(business$restaurant, by=list(city=business$city), FUN=sum)
-large_cities <- subset(agg, x > 500)$city
-small_cities <- subset(agg, x <= 500)$city
+large_cities <- subset(agg, x >= 1000)$city
+small_cities <- subset(agg, x < 1000)$city
 business$city_type <- unlist(lapply(business$city, function (x) {ifelse(x %in% large_cities, 1, 0)}))
+
+# Updated to extract restaurant file for global and restaurant file for US Only
+agg_lg_cities <- aggregate(business$restaurant[business$city %in% large_cities], by=list(city=business$city[business$city %in% large_cities]), FUN=sum)
+agg_lg_cities <- agg_lg_cities[order(agg_lg_cities$x, decreasing = TRUE),]
+agg_gbl_cities <- agg_lg_cities$city[agg_lg_cities$city %in% c("Montréal", "Pittsburgh", "Edinburgh", "Cleveland", "Stuttgart")]
+agg_us_cities <- agg_lg_cities$city[agg_lg_cities$city %in% c("Las Vegas", "Phoenix", "Charlotte", "Pittsburgh", "Scottsdale", "Cleveland", "Mesa")]
+business$global_restaurant <- ifelse(business$city %in% agg_gbl_cities & business$restaurant == 1, 1, 0)
+business$us_restaurant <- ifelse(business$city %in% agg_us_cities & business$restaurant == 1, 1, 0)
 
 # Output files
 print("output processed business csv")
@@ -42,6 +50,12 @@ write.csv(business[, !(names(business) == "categories_l")], "../../data/processe
 
 print("output new restaurants csv")
 write.csv(business[business$restaurant == 1 & business$city_type == 1, !(names(business) == "categories_l")], "../../data/processed/restaurants.csv")
+
+print("output new global restaurants csv")
+write.csv(business[business$global_restaurant == 1, !(names(business) == "categories_l")], "../../data/processed/global_restaurants.csv")
+
+print("output new us only restaurants csv")
+write.csv(business[business$us_restaurant == 1, !(names(business) == "categories_l")], "../../data/processed/us_restaurants.csv")
 
 # QA against exploratory restaurant counts
 # agg <- aggregate(business$restaurant, by=list(city=business$city), FUN=sum)
