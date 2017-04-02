457eaefddc35ed80dd77436e5fba9b2b815b420b
michmcc <michmcc@me.com>
2017-04-02 15:36:55 -0400

    Updates to Clean Reviews and Topic Modeling
    Updated the pre-text mining cleaning that is done in
    clean_review_data.R - it now outputs a training bi-gram document term
    matrix (cpdtm_unstem_bi_train.RData) as well as a full one
    (cpdtm_unstem_bi.RData). The training one only includes 5000 random
    reviews per city so that is is balanced (e.g. Las Vegas doesn’t
    dominate the corpus). Those DTM files are read by topic_modelling.R
    which outputs for each restaurant the counts of the reviews belonging
    to each topics (business_topics.csv)

--- src/models/topic_modelling.R
+++ src/models/topic_modelling.R
@@ -5,14 +5,73 @@
 #################
 
 library(topicmodels)
+#library(dplyr)
+library(stringr)
+
 
 ## load previous document term matrix
-load("../../data/processed/cpdtm.RData")
+#load("../../data/processed/cpdtm.RData")
+
+# load unstemmed document term matrix
+#load("../../data/processed/cpdtm_unstem.RData")
+
+# load TRAINING unstemmed BIGRAM document term matrix
+load("../../data/processed/cpdtm_unstem_bi_train.RData")
+
+# load FULL
+load("../../data/processed/cpdtm_unstem_bi.RData")
+
 
 ## LDA
 ## set 5 topics
-k <- 5
-output <- LDA(cpdtm, k)
+#k <- 5
+#output <- LDA(cpdtm, k)
+
+
+
 
 ## show 10 terms associated with each model
-terms(output, 10)
+#terms(output, 10)
+
+## trying on unstemmed data
+
+## set 5 topics
+#k <- 3
+#output <- LDA(cpdtm_unstem, k, method = "Gibbs")
+
+
+## bigram unstemmed data
+
+## set 3 topics
+set.seed(1234)
+k <- 3
+output <- LDA(cpdtm_unstem_bi_train, k, control=list(seed=0), method = "Gibbs")
+
+# output words per topic
+terms(output, 15)
+
+# assign topic to each review in the FULL training set
+full.topics <- posterior(output,cpdtm_unstem_bi)
+full.topics <- apply(full.topics$topics, 1, which.max)
+
+review_topics <- as.data.frame(full.topics)
+review_topics <- data.frame(business_review_id = rownames(review_topics), review_topics, row.names = NULL)
+
+review_topics$business_id <- str_split_fixed(review_topics$business_review_id, "/", 2)[,1]
+review_topics$review_id <- str_split_fixed(review_topics$business_review_id, "/", 2)[,2]
+
+review_topics <- review_topics[, c("business_id", "review_id", "full.topics")]
+
+business_topic_cnt <- as.data.frame.matrix(table(review_topics$business_id, review_topics$full.topics))
+business_topic_cnt <- data.frame(business_id = rownames(business_topic_cnt), business_topic_cnt, row.names = NULL)
+colnames(business_topic_cnt) <- c("business_id", "topic_1", "topic_2", "topic_3")
+
+business_review_cnt <- as.data.frame(table(review_topics$business_id))
+colnames(business_review_cnt) <- c("business_id", "review_cnt")
+
+business_topics <- merge(business_review_cnt, business_topic_cnt)
+
+
+## output for each business, the proportion of reviews corresponding to each topic
+write.csv(business_topics, file = "../../data/processed/business_topics.csv")
+
